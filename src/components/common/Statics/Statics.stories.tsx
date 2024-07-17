a79beefac289bb1256dda6281c905a6438708c89
obinox <parkjonggu1122@gmail.com>
2024-07-17 16:01:28 +0900

    feat: 스트로크 추가, 꺾은선 추가

--- src/components/common/Statics/Statics.stories.tsx
+++ src/components/common/Statics/Statics.stories.tsx
@@ -12,7 +12,25 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
+const array: number[] = Array(0);
+for (let i = 0; i < 9; i++) {
+  array.push(Math.random() * 100 + 100);
+}
 export const Default: Story = {
-  args: {},
+  args: {
+    values: array,
+    maxWidth: 800,
+    maxHeight: 1200,
+    viewSize: 0.4,
+    maxMaxHeight: 0.8,
+    rectFill: "#ADADAD",
+    rectStrokeFill: "#858585",
+    rectStrokeWidth: 0.2,
+    circleRadius: 0.4,
+    circleFill: "#9A9A9A",
+    circleStrokeFill: "#454545",
+    circleStrokeWidth: 0.3,
+    pathStroke: "#BCBCBC",
+    pathWidth: 10,
+  },
 };
--- src/components/common/Statics/Statics.tsx
+++ src/components/common/Statics/Statics.tsx
@@ -3,36 +3,101 @@ for (let i = 0; i < 127; i++) {
   data.push(i);
 }
 
-export function Statics({}) {
-  const ar: number[] = Array(0);
-  const MAXH = 1000;
-  const MAXW = 1200;
-  const VIEW = 0.4;
-  for (let i = 0; i < 9; i++) {
-    ar.push(Math.random() * 100 + 100);
-  }
-  const Max = Math.max(...ar);
-  const count = ar.length;
+export function Statics({
+  values,
+  maxWidth,
+  maxHeight,
+  viewSize,
+  maxMaxHeight,
+  rectFill,
+  rectStrokeFill,
+  rectStrokeWidth,
+  circleRadius,
+  circleFill,
+  circleStrokeFill,
+  circleStrokeWidth,
+  pathStroke,
+  pathWidth,
+}: {
+  values: number[];
+  maxWidth: number;
+  maxHeight: number;
+  viewSize: number;
+  maxMaxHeight: number;
+  rectFill: string;
+  rectStrokeFill: string;
+  rectStrokeWidth: number;
+  circleRadius: number;
+  circleFill: string;
+  circleStrokeFill: string;
+  circleStrokeWidth: number;
+  pathStroke: string;
+  pathWidth: number;
+}) {
+  const Max = Math.max(...values);
+  const count = values.length;
   return (
     <>
       <svg
         id="canv"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox={`0 0 ${MAXW} ${MAXH}`}
+        viewBox={`0 0 ${maxHeight} ${maxWidth}`}
         fill="none"
-        width={MAXW * VIEW}
-        height={MAXH * VIEW}
+        height={maxWidth * viewSize}
+        width={maxHeight * viewSize}
       >
-        {ar.map((e, i) => {
+        {values.map((e, i) => {
           return (
             <>
               <rect
-                x={((i * MAXW) / (count * 2 + count - 1)) * 3}
-                y={MAXH - (e / Max) * MAXH * 0.8}
-                height={(e / Max) * MAXH * 0.8}
-                width={(MAXW / (count * 2 + count - 1)) * 2}
-                fill="#000000"
+                x={
+                  ((i * maxHeight) / (count * 2 + count - 1)) * 3 +
+                  ((maxHeight / (count * 2 + count - 1)) * rectStrokeWidth) / 2
+                }
+                y={
+                  maxWidth -
+                  (e / Max) * maxWidth * maxMaxHeight +
+                  ((maxHeight / (count * 2 + count - 1)) * rectStrokeWidth) / 2
+                }
+                height={
+                  (e / Max) * maxWidth * maxMaxHeight -
+                  ((maxHeight / (count * 2 + count - 1)) * 2 * rectStrokeWidth) / 2
+                }
+                width={
+                  (maxHeight / (count * 2 + count - 1)) * 2 -
+                  ((maxHeight / (count * 2 + count - 1)) * 2 * rectStrokeWidth) / 2
+                }
+                fill={rectFill}
+                stroke={rectStrokeFill}
+                strokeWidth={((maxHeight / (count * 2 + count - 1)) * 2 * rectStrokeWidth) / 2}
               ></rect>
+            </>
+          );
+        })}
+        <path
+          stroke={pathStroke}
+          strokeWidth={pathWidth}
+          d={values
+            .map((e, i) => {
+              return `${i == 0 ? "M" : "L"} ${(((i + 1 / 3) * maxHeight) / (count * 2 + count - 1)) * 3} ${maxWidth - (e / Max) * maxWidth * maxMaxHeight} `;
+            })
+            .join("")}
+        ></path>
+        {values.map((e, i) => {
+          return (
+            <>
+              <circle
+                cx={(((i + 1 / 3) * maxHeight) / (count * 2 + count - 1)) * 3}
+                cy={maxWidth - (e / Max) * maxWidth * maxMaxHeight}
+                r={
+                  (maxHeight / (count * 2 + count - 1)) * circleRadius * (1 - circleStrokeWidth / 2)
+                }
+                fill={circleFill}
+                stroke={circleStrokeFill}
+                strokeWidth={
+                  ((maxHeight / (count * 2 + count - 1)) * circleRadius * 2 * circleStrokeWidth) / 2
+                }
+              ></circle>
             </>
           );
         })}
