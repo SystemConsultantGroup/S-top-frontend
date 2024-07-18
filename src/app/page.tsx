4df8901265acc97ae3a61ad1404de51a92aea547
hynseok <wefwef12e@naver.com>
2024-07-18 15:39:55 +0900

    feat(components): 프로젝트 카드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { MockProjectData } from "@/components/common/ProjectCard/_mock/mock-project";
+import { ProjectCard } from "@/components/common/ProjectCard/ProjectCard";
+
 export default function Home() {
-  return <main>Hello, world!</main>;
+  const data = MockProjectData;
+  return (
+    <main>
+      <ProjectCard data={data} />
+    </main>
+  );
 }
