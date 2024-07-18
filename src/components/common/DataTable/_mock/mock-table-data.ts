9438706286178521692d491ae778e458506d8899
hynseok <wefwef12e@naver.com>
2024-07-18 23:34:43 +0900

    feat(mockups): 데이터 테이블 목업데이터 추가

---
+++ src/components/common/DataTable/_mock/mock-table-data.ts
@@ -0,0 +1,84 @@
+export type MockUserType = {
+  id: number;
+  name: string;
+  email: string;
+  belonging: string;
+  position: string;
+  role: string;
+  createdAt: string;
+};
+
+export const MockUsersData: MockUserType[] = [
+  {
+    id: 1,
+    name: "이종민",
+    email: "jmlee@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "삼종민",
+    email: "jmsam@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "사종민",
+    email: "jmsaa@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "오종민",
+    email: "jmooo@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "육종민",
+    email: "jmyuk@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "칠종민",
+    email: "jmqil@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "팔종민",
+    email: "jmpal@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+  {
+    id: 1,
+    name: "구종민",
+    email: "jmgoo@skku.edu",
+    belonging: "시스템컨설턴트그룹SCG",
+    position: "개발자",
+    role: "관리자",
+    createdAt: "2024-07-08T11:59:02",
+  },
+];
