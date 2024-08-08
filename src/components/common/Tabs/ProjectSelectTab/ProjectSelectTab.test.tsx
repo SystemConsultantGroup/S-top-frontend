6ae75270d964ab709e47c7814e058b2760da8c7c
sera2002 <ouioui2@g.skku.edu>
2024-08-08 23:17:49 +0900

    fix: content 네이밍 변경

--- src/components/common/Tabs/ProjectSelectTab/ProjectSelectTab.test.tsx
+++ src/components/common/Tabs/ProjectSelectTab/ProjectSelectTab.test.tsx
@@ -5,8 +5,8 @@ import "@testing-library/jest-dom";
 describe("Project Select Tab component", () => {
   it("renders the correct default tab", () => {
     const tabs: TabType[] = [
-      { id: "1", label: "Tab 1", content: "Tab 1 content" },
-      { id: "2", label: "Tab 2", content: "Tab 2 content" },
+      { id: "1", label: "Tab 1", children: "Tab 1 content" },
+      { id: "2", label: "Tab 2", children: "Tab 2 content" },
     ];
     render(<ProjectSelectTab tabs={tabs} defaultTabId={"2"} />);
 
@@ -15,9 +15,9 @@ describe("Project Select Tab component", () => {
 
   it("renders all the tab labels provided", () => {
     const tabs = [
-      { id: "1", label: "Tab 1", content: "Tab 1 content" },
-      { id: "2", label: "Tab 2", content: "Tab 2 content" },
-      { id: "3", label: "Tab 3", content: "Tab 3 content" },
+      { id: "1", label: "Tab 1", children: "Tab 1 content" },
+      { id: "2", label: "Tab 2", children: "Tab 2 content" },
+      { id: "3", label: "Tab 3", children: "Tab 3 content" },
     ];
     render(<ProjectSelectTab tabs={tabs} defaultTabId={"1"} />);
 
