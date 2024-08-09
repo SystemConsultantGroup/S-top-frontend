6dc96ec03632109085a2ade74a04866367aa7b6e
김현석 <wefwef12e@naver.com>
2024-08-09 19:15:28 +0900

    [FEAT] 프로젝트 카드 컴포넌트 추가 (#18)
    * chore: 테마 색상 수정 (다크모드 반영)
    
    * feat(components): 프로젝트 카드 추가

---
+++ src/components/common/ProjectCard/ProjectCard.story.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ProjectCard } from "./ProjectCard";
+import { MockProjectData } from "./_mock/mock-project";
+
+const meta = {
+  title: "ProjectCard",
+  component: ProjectCard,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof ProjectCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const data = MockProjectData;
+
+export const Usage: Story = {
+  args: {
+    data: data,
+  },
+};
