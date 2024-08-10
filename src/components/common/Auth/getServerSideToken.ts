0165242229b7bb6d31e73587507ee9f7fa7574e6
hynseok <wefwef12e@naver.com>
2024-08-10 21:55:01 +0900

    fix: getServerSideToken refresh 토큰도 가져올 수 있도록 수정

--- src/components/common/Auth/getServerSideToken.ts
+++ src/components/common/Auth/getServerSideToken.ts
@@ -1,11 +1,12 @@
 "use server";
 
-import { JWT_COOKIE_NAME } from "@/constants/Auth";
+import { JWT_COOKIE_NAME, REFRESH_TOKEN_NAME } from "@/constants/Auth";
 import { cookies } from "next/headers";
 
 export async function getServerSideToken() {
   const cookie = cookies();
-  const token = cookie.get(JWT_COOKIE_NAME)?.value || null;
+  const accessToken = cookie.get(JWT_COOKIE_NAME)?.value || null;
+  const refreshToken = cookie.get(REFRESH_TOKEN_NAME)?.value || null;
 
-  return Promise.resolve(token);
+  return Promise.resolve({ accessToken, refreshToken });
 }
