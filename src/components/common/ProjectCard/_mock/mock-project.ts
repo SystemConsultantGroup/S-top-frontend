079b8e940b705bcbdd8c94f8b5c5b34d93b68a77
chunzhi23 <05.huanx10ng@gmail.com>
2024-08-10 15:20:51 +0900

    [FEAT] 프로젝트 카드 컴포넌트 추가

---
+++ src/components/common/ProjectCard/_mock/mock-project.ts
@@ -0,0 +1,13 @@
+import { ProjectCardDataType } from "../ProjectCard";
+
+export const MockProjectData: ProjectCardDataType = {
+  id: 1,
+  title: "GPT 기반의 일상 대화형 챗봇 모델 개발",
+  thumbnailUrl: "/images/mock-project-thumbnail.png",
+  categories: ["AI", "자연어처리"],
+  participants: ["조민규", "조하빈", "장준우", "김예윤", "신준서"],
+  team: "바이브컴퍼니",
+  advisor: "박희선",
+  likes: 63,
+  isMarked: true,
+};
