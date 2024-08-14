a9191c85350f405d42eccb108605e6aa581c485b
Joo Won <110383412+chqkq@users.noreply.github.com>
2024-08-14 17:19:40 +0900

    [FEAT] 관리자 사이드바 추가

---
+++ src/components/pages/AdminSidebar/AdminSidebar.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "@/components/common/Sidebar";
+import { AdminSidebarMenu } from "./AdminSidebarMenu";
+
+export function AdminSidebar() {
+  return (
+    <Sidebar>
+      <AdminSidebarMenu />
+    </Sidebar>
+  );
+}
