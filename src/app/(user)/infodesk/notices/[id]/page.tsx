358f031c30d6457ad00bacfaf15d4f0f4b04f4bd
chunzhi23 <05.huanx10ng@gmail.com>
2024-08-15 10:10:38 +0900

    [FEAT] #44 공지사항 세부 페이지

--- src/app/(user)/infodesk/notices/[id]/page.tsx
+++ src/app/(user)/infodesk/notices/[id]/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { SubHeadNavbar } from "@/components/common/SubHeadNavbar";
+import styles from "./styles.module.css";
+import { NoticeDetail } from "@/components/common/NoticeDetail";
+import { getUserNoticeDetail } from "./GetUserNoticeDetail";
+import { useSelectedLayoutSegment } from "next/navigation";
+
 export default function NoticeDetailPage() {
-  return <main>Hello, world!</main>;
+  const segment = useSelectedLayoutSegment();
+
+  const { children, ...othersDetailProps } = getUserNoticeDetail(segment);
+
+  return (
+    <>
+      <SubHeadNavbar title="Info Desk" />
+      <div className={styles.container}>
+        <NoticeDetail heading="공지사항" {...othersDetailProps}>
+          {children}
+        </NoticeDetail>
+      </div>
+    </>
+  );
 }
