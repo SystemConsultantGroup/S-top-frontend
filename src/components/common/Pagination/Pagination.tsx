4d1c8bc282900b186332c2e10aae2cc3d9408f4f
obinox <parkjonggu1122@gmail.com>
2024-08-16 17:11:15 +0900

    feat: 페이지네이션 강화 ★☆☆

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -9,15 +9,25 @@ import {
 import classes from "./Pagination.module.css";
 import { useState } from "react";
 
-export function Paginations({ show, data, ...props }: { show: number; data: Array<Element> }) {
+export function Paginations({
+  data,
+  count,
+  pinned = [],
+  ...props
+}: {
+  pinned?: Array<Element>;
+  data: Array<Element>;
+  count: number;
+}) {
+  const count2 = count - pinned.length;
   const [page, setPage] = useState(1);
-  const pages = data.slice((page - 1) * show, page * show);
+  const pages = pinned.concat(data.slice((page - 1) * count2, page * count2));
   return (
     <>
       {pages}
 
       <PaginationRoot
-        total={Math.ceil(data.length / show)}
+        total={Math.ceil(data.length / count2)}
         styles={{
           root: { borderStyle: "none" },
           control: { borderStyle: "none" },
