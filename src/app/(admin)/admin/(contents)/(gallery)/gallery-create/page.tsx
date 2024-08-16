d28dcb0f73314a5493f96fbd2581f823750cbb8d
obinox <parkjonggu1122@gmail.com>
2024-08-16 14:53:17 +0900

    feat: 통계 경로

--- src/app/(admin)/admin/(contents)/(gallery)/gallery-create/page.tsx
+++ src/app/(admin)/admin/(contents)/(gallery)/gallery-create/page.tsx
@@ -1,4 +1,4 @@
-export default function AdminGalleryPage() {
+export default function AdminGalleryCreate() {
   return (
     <>
       <div>
---
+++ src/app/(admin)/admin/(statics)/accesses/page.tsx
@@ -0,0 +1,12 @@
+// import { Statics } from "@/components/common/Statics/Statics";
+
+export default function AdminAccessesStatics() {
+  return (
+    <>
+      <div>
+        <h1>접속 통계</h1>
+      </div>
+      {/* <div><Statics></Statics></div> */}
+    </>
+  );
+}
