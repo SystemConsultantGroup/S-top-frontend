6952da06eca3a604a3f3cd9970fc0423e5f2a077
sera2002 <ouioui2@g.skku.edu>
2024-08-17 12:50:37 +0900

    feat: primary button에서 classname 받아올 수 있도록 수정

--- src/components/common/Buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/common/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,12 +1,11 @@
 import { Button, ButtonProps } from "@mantine/core";
+import classNames from "classnames";
 import classes from "./PrimaryButton.module.css";
 
-export function PrimaryButton({ label, ...props }: ButtonProps & { label?: string }) {
+export function PrimaryButton({ label, className, ...props }: ButtonProps & { label?: string }) {
   return (
-    <>
-      <Button className={classes.element} {...props}>
-        {label}
-      </Button>
-    </>
+    <Button className={classNames(classes.element, className)} {...props}>
+      {label}
+    </Button>
   );
 }
