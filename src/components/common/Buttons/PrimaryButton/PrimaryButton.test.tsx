5d262022edf067c95529e72a019bb14f6a4702b4
sera2002 <ouioui2@g.skku.edu>
2024-08-18 21:37:40 +0900

    fix: 버튼 컴포넌트 사용성 개선

--- src/components/common/Buttons/PrimaryButton/PrimaryButton.test.tsx
+++ src/components/common/Buttons/PrimaryButton/PrimaryButton.test.tsx
@@ -4,9 +4,16 @@ import "@testing-library/jest-dom";
 
 describe("PrimaryButton component", () => {
   it("renders correctly with the given label", () => {
-    render(<PrimaryButton label="Button" />);
+    render(<PrimaryButton>Button</PrimaryButton>);
     // More on screen queries: https://testing-library.com/docs/queries/about
     // More on jest expect Api: https://jestjs.io/docs/expect
     expect(screen.getByRole("button", { name: "Button" })).toBeInTheDocument();
+  });
+
+  it("applies the given className", () => {
+    const testClassName = "test-class";
+    render(<PrimaryButton className={testClassName}>Button</PrimaryButton>);
+
+    expect(screen.getByRole("button", { name: "Button" })).toHaveClass(testClassName);
   });
 });
