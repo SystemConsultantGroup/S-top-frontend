cc872ac2835e4c92984289c2ce4edee346cd950b
sera2002 <ouioui2@g.skku.edu>
2024-08-28 00:23:09 +0900

    feat: 수상 결과 페이지 추가

--- src/app/(user)/event/award/page.tsx
+++ src/app/(user)/event/award/page.tsx
@@ -1,3 +1,12 @@
+import { Text } from "@mantine/core";
+import { EventAwardView } from "@/components/pages/EventAwardView/EventAwardView";
+import classes from "./page.module.css";
+
 export default function EventAwardPage() {
-  return <main>Hello, world!</main>;
+  return (
+    <div className={classes.container}>
+      <Text className={classes.title}>작품 수상 결과</Text>
+      <EventAwardView />
+    </div>
+  );
 }
---
+++ src/components/pages/EventAwardView/_mock/mock-awards.ts
@@ -0,0 +1,6 @@
+import { MockProjectData } from "@/components/common/ProjectCard/_mock/mock-project";
+
+export const years = ["2023", "2022", "2021", "2020"];
+
+export const awards = ["대상", "최우수상", "우수상", "우수상", "장려상", "장려상", "장려상"];
+export const projects = new Array(7).fill(MockProjectData);
