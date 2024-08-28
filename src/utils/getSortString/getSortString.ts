15178eaca672733e906fb6e7c47ec97882e0834c
hynseok <wefwef12e@naver.com>
2024-08-28 18:44:40 +0900

    feat: 정렬 쿼리파라미터 문자열 생성 유틸함수 추가

--- src/utils/getSortString/getSortString.ts
+++ src/utils/getSortString/getSortString.ts
@@ -4,6 +4,6 @@
  * @param order 정렬 방향 (0: 오름차순, 1: 내림차순)
  * @returns 정렬 문자열
  */
-export function getSortString({ sortBy, order }: { sortBy: string; order: number }) {
+export function getSortString({ sortBy, order }: { sortBy?: string; order?: number }) {
   return `${sortBy},${order === 0 ? "asc" : "desc"}`;
 }
