f4cea2e3b4389bedcd907a346d2fb17385cca6d6
chunzhi23 <05.huanx10ng@gmail.com>
2024-08-28 21:50:30 +0900

    [FIX] 텍스트 입력 수정

--- src/components/common/TextInput/TextInput.tsx
+++ src/components/common/TextInput/TextInput.tsx
@@ -1,11 +1,13 @@
 import { Input, InputProps, InputWrapper } from "@mantine/core";
 import { ReactNode } from "react";
+import styles from "./TextInput.module.css";
 
 type TextInputPropsType = {
   label?: string | ReactNode;
   description?: string | ReactNode;
   error?: string | ReactNode;
   placeholder?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export function TextInput({
@@ -13,11 +15,19 @@ export function TextInput({
   description,
   error,
   placeholder,
+  onChange,
   ...props
 }: InputProps & TextInputPropsType) {
   return (
     <InputWrapper label={label} description={description} error={error}>
-      <Input placeholder={placeholder} {...props} />
+      <Input
+        classNames={{
+          input: styles.input,
+        }}
+        placeholder={placeholder}
+        onChange={onChange}
+        {...props}
+      />
     </InputWrapper>
   );
 }
