091ebfbb8a935827973655aa1bcbc6288c82475d
chunzhi23 <05.huanx10ng@gmail.com>
2024-08-28 14:47:19 +0900

    appTheme 변경 색상 적용 및 handler 추가

--- src/components/common/SearchInput/SearchInput.test.tsx
+++ src/components/common/SearchInput/SearchInput.test.tsx
@@ -1,10 +1,36 @@
 import { render, screen } from "@/utils/test-utils";
 import { SearchInput } from "./SearchInput";
 import "@testing-library/jest-dom";
+import { ChangeEvent, KeyboardEvent, MouseEvent, useState } from "react";
 
 describe("SearchInput component", () => {
   it("renders correctly with the given placeholder", () => {
+    const [value, setValue] = useState("");
+
+    // Event Handlers
+    // Handles input changes and clear actions
+    const handleInput = (e: MouseEvent | ChangeEvent, payload?: unknown) => {
+      if (e.type === "change") {
+        const target = e.target as HTMLInputElement;
+        setValue(() => target.value);
+      } else if (payload) {
+        if (payload === "CLEAR") {
+          setValue(() => "");
+        }
+      }
+    };
+
+    // Handles "Enter" key press for search
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.code === "Enter") {
+        console.log("Enter action makes server to search!");
+      }
+    };
+
     const props = {
+      value,
+      handleInput,
+      handleKeyDown,
       placeholder: "Input here to search...",
       iconSize: 16,
     };
--- src/components/common/SearchInput/SearchInput.tsx
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -1,22 +1,47 @@
-import { Input, InputProps } from "@mantine/core";
+import { CloseButton, Input, InputProps } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
+import { ChangeEvent, KeyboardEvent, MouseEvent } from "react";
 import styles from "./SearchInput.module.css";
 
+interface ISearchHandler {
+  value: string;
+  handleInput: (event: MouseEvent | ChangeEvent, payload?: unknown) => void;
+  handleKeyDown: (event: KeyboardEvent, payload?: unknown) => void;
+}
+
+type SearchInputProps = InputProps &
+  ISearchHandler & {
+    placeholder?: string;
+    iconSize?: number;
+  };
+
 export function SearchInput({
+  value,
+  handleInput,
+  handleKeyDown,
   placeholder,
   iconSize = 16,
   ...props
-}: InputProps & { placeholder?: string; iconSize?: number }) {
+}: SearchInputProps) {
   return (
     <>
       <Input
         classNames={{
           input: styles.input,
           section: styles.section,
-          wrapper: styles.wrapper,
         }}
+        value={value}
         placeholder={placeholder}
+        onChange={handleInput}
+        onKeyDown={handleKeyDown}
+        rightSectionPointerEvents="all"
         leftSection={<IconSearch size={iconSize} />}
+        rightSection={
+          <CloseButton
+            onClick={(e) => handleInput(e, "CLEAR")}
+            style={{ display: value ? undefined : "none" }}
+          />
+        }
         {...props}
       />
     </>
