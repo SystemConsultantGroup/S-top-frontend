f3b8d162c53f1dda6bb29a0a63292e76f2613bfc
hynseok <wefwef12e@naver.com>
2024-09-03 11:04:54 +0900

    fix: 메인페이지 롤백

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,26 +1,3 @@
-"use client";
-
-import { CommonAxios } from "@/utils/CommonAxios";
-import { Button } from "@mantine/core";
-
 export default function Home() {
-  const body = {
-    name: "stop-user",
-    phoneNumber: "010-1234-1234",
-    userType: "INACTIVE_PROFESSOR",
-    email: "email@gmail.com",
-    signUpSource: "ad",
-    division: null,
-    position: null,
-  };
-
-  return (
-    <main>
-      <Button
-        onClick={() => {
-          CommonAxios.post("/auth/register", body);
-        }}
-      />
-    </main>
-  );
+  return <main>Hello, world!</main>;
 }
