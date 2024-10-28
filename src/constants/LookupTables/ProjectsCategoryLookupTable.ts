b5cc247bb7029a5c3da1958d3ef816f77a00443c
hynseok <wefwef12e@naver.com>
2024-10-28 11:10:12 +0900

    feat: 프로젝트 종류, 타입 lookup테이블 추가

---
+++ src/constants/LookupTables/ProjectsCategoryLookupTable.ts
@@ -0,0 +1,10 @@
+export const ProjectsCategoryLookupTable = {
+  "시스템/네트워크": "SYSTEM_NETWORK",
+  "웹/애플리케이션": "WEB_APPLICATION",
+  컴퓨터비전: "COMPUTER_VISION",
+  "보안/소프트웨어공학": "SECURITY_SOFTWARE_ENGINEERING",
+  "AI/머신러닝": "AI_MACHINE_LEARNING",
+  자연어처리: "NATURAL_LANGUAGE_PROCESSING",
+  빅데이터분석: "BIG_DATA_ANALYSIS",
+  "인터랙션/증강현실": "INTERACTION_AUGMENTED_REALITY",
+};
