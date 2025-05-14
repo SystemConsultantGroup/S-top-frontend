c049ec11866b396994f9767cc4ba83f7eacfa5e6
o-bard-o <i4song.dev@gmail.com>
2025-05-14 21:39:39 +0900

    fix: Fix minor UI design :lipstick:

--- src/components/common/ProjectCard/ProjectCard.tsx
+++ src/components/common/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardSection, Divider, Group, Stack } from "@mantine/core";
+import { Box, Card, CardSection, Divider, Group, Stack, Text } from "@mantine/core";
 import Image from "next/image";
 import classes from "./ProjectCard.module.css";
 import { IconSearch } from "@tabler/icons-react";
@@ -7,7 +7,6 @@ import { ProjectCardLikeSection } from "./ProjectCardLikeSection";
 import { CardBadge } from "../CardBadge";
 import { IProjectContent } from "@/types/project";
 import { PROJECT_CATEGORY_MAPPED_LIST } from "@/constants/TextMapping";
-import { Text } from '@mantine/core';
 
 export interface ProjectCardProps {
   data: IProjectContent;
@@ -64,10 +63,10 @@ export function ProjectCard({
             }}
             style={{ textDecorationLine: "none" }}
           >
-            <Text 
+            <Text
               h={studentsString.length > 0 ? 28 : 56} // 28: lineHeight, 56: 2 lines' height
-              lineClamp={studentsString.length > 0 ? 1 : 2} 
-              component="div" 
+              lineClamp={studentsString.length > 0 ? 1 : 2}
+              component="div"
               className={classes.title}
             >
               {data.projectName}
@@ -83,15 +82,14 @@ export function ProjectCard({
             <div className={classes["attr-wrapper"]}>참가팀명</div>
             <div className={classes["value-wrapper"]}>{data.teamName}</div>
           </Group>
-          {professorString.length > 0 ?
-            (<Group gap={24}>
+          {professorString.length > 0 ? (
+            <Group gap={24}>
               <div className={classes["attr-wrapper"]}>지도교수</div>
               <div className={classes["value-wrapper"]}>{professorString}</div>
-            </Group>) : 
+            </Group>
+          ) : (
             <Box h={24} />
-          }
-
-          
+          )}
         </Stack>
       </CardSection>
       <ProjectCardLikeSection
